8cf0fb55cab6cb0ee3f774387a3877557daf7363
lleellee0 <lleellee013@gmail.com>
2019-01-07 01:22:51 +0900

    config_utils로 config 관련 함수 분리 // 기본 기능 완성

---
+++ config_utils.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const saveConfig = (config) => {
+    let subscriber_ids_arr = [];
+    let subscriber_ids_set = config.subscriber_ids;    // set 임시 저장
+    config.subscriber_ids.forEach(v => subscriber_ids_arr.push(v));
+    config.subscriber_ids = subscriber_ids_arr;
+
+    fs.writeFile('config.json', JSON.stringify(config), (err, data) => {
+        if (err)
+            throw err;
+        config.subscriber_ids = subscriber_ids_set; // set 객체 원복
+    });
+}
+
+const loadConfig = (config) => {
+    let subscriber_ids_set = new Set();
+    config.subscriber_ids.forEach((v) => subscriber_ids_set.add(v));
+    config.subscriber_ids = subscriber_ids_set;
+}
+
+module.exports = {
+    saveConfig: saveConfig,
+    loadConfig: loadConfig
+}
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const configUtils = require('./config_utils.js');
 const config = require('./config.json');
 
-console.log(config);
+configUtils.loadConfig(config);
 
 const TeleBot = require('telebot');
 let bot = new TeleBot(config.api_key);
 
-bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
+// 처음 입장시
+bot.on(['/start', '/hello'], (msg) => {
+    config.subscriber_ids.add(msg.from.id);
+    configUtils.saveConfig(config);
+    return msg.reply.text(`어서오세요! 새로운 구독자를 추가했습니다.
+    다음과 같은 명령어를 사용하실 수 있습니다.
+    
+    /naver_webtoon : 네이버 웹툰 유료화 알림
+    /etc : 기타 등등`);
+});
+
 bot.start();
