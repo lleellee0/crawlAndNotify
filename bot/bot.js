bf6dd59b1d5436eb9d1054a8c748d15dfe11bf3a
lleellee0 <lleellee013@gmail.com>
2019-03-10 00:03:08 +0900

    주석제거

--- bot/bot.js
+++ bot/bot.js
@@ -1,9 +1,4 @@
 // https://www.npmjs.com/package/telebot 참조
-// ** 중요
-// config.json이라는 이름의 파일에 다음 내용이 들어있도록 파일을 새로 만들어주세요.
-//
-// {"api_key":"YOUR_TOKEN","subscriber_ids":[]}
-// YOUR_TOKEN이 있는 부분에는 발급받은 API키를 입력해주세요.
 
 const configUtils = require('../conf/config_utils.js');
 const config = require('../conf/config.json');
